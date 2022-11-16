bf9196ed853faac65a1f452f3a70b31394723b8e
Nia292 <73426965+Nia292@users.noreply.github.com>
2022-11-16 15:26:50 +0100

    Updated to now log version, too

--- gateway.js
+++ gateway.js
@@ -5,26 +5,8 @@ const crypto = require('crypto');
 const app = express()
 const port = 3000
 
-app.get('/proxy-request', async (req, res) => {
-    const requestId = crypto.randomUUID();
-    console.log(`[${requestId}] Receiving proxy request from ${req.hostname}`)
-    const webhookTarget = req.query.webhook;
-    const webhookMessage = req.query.message;
-    if (!webhookTarget) {
-        console.log(`[${requestId}] Query parameter "webhook" not provided.`)
-        res.status(400)
-        res.contentType("application/json")
-        res.send(JSON.stringify({error: 'Query parameter "webhook" not provided.'}));
-        return;
-    }
-    if (!webhookMessage) {
-        console.log(`[${requestId}] Query parameter "message" not provided.`)
-        res.status(400)
-        res.contentType("application/json")
-        res.send(JSON.stringify({error: 'Query parameter "message" not provided.'}));
-        return;
-    }
-    console.log(`[${requestId}] Message is ${webhookMessage}`)
+async function handleVersionOne(req, res, requestId, webhookMessage, webhookTarget, version) {
+    console.log(`[${requestId}] Version is ${version}`)
     const discordRequest = {
         content: webhookMessage,
     };
@@ -41,6 +23,29 @@ app.get('/proxy-request', async (req, res) => {
     console.log(`[${requestId}] Proxy done.`)
     res.send(JSON.stringify({"result": 'ok'}));
     return true;
+}
+
+app.get('/proxy-request', async (req, res) => {
+    const requestId = crypto.randomUUID();
+    console.log(`[${requestId}] Receiving proxy request from ${req.socket.remoteAddress}`)
+    const webhookTarget = req.query.webhook;
+    const webhookMessage = req.query.message;
+    const version = req.query.twVersion;
+    if (!webhookTarget) {
+        console.log(`[${requestId}] Query parameter "webhook" not provided.`)
+        res.status(400)
+        res.contentType("application/json")
+        res.send(JSON.stringify({error: 'Query parameter "webhook" not provided.'}));
+        return;
+    }
+    if (!webhookMessage) {
+        console.log(`[${requestId}] Query parameter "message" not provided.`)
+        res.status(400)
+        res.contentType("application/json")
+        res.send(JSON.stringify({error: 'Query parameter "message" not provided.'}));
+        return;
+    }
+    return handleVersionOne(req, res, requestId, webhookMessage, webhookTarget, version);
 })
 
 app.listen(port, () => {
